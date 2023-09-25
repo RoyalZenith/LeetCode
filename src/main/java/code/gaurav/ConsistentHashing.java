7533a35cf98a03521da7be7fc6e1ce9f4ef3daa0
gauravsinghal <gauravsingha10071998@gmail.com>
2023-09-26 00:21:07 +0530

    Added new problem

--- src/main/java/code/gaurav/ConsistentHashing.java
+++ src/main/java/code/gaurav/ConsistentHashing.java
@@ -3,148 +3,95 @@ package code.gaurav;
 import java.util.*;
 
 public class ConsistentHashing {
-    public ArrayList<Integer> solve(ArrayList<String> A, ArrayList<String> B, ArrayList<Integer> C) {
-        List<Set<Integer>> machines = new ArrayList<>();
-        Map<String,Integer> mp = new HashMap<>();
-        for(int i=0;i<360;i++){
-            machines.add(i,null);
-        }
-        ArrayList<Integer> ans = new ArrayList<>();
-        for(int i=0;i<A.size();i++){
-            int hashValue= userHash(B.get(i),C.get(i));
-            switch(A.get(i)){
-                case "ADD":
-                    if(mp.containsValue(hashValue)){
-                        mp.put(B.get(i),hashValue);
+        Map<String, List<Integer>> serverData = new HashMap<>(); // mapping of server and their user list
+        TreeMap<Integer, String> server = new TreeMap<>(); // mapping of hashval and server name
+        Map<String, Integer> mapServer = new HashMap<>(); //mapping of server name and hashVal
+        public ArrayList<Integer> solve(ArrayList<String> A, ArrayList<String> B, ArrayList<Integer> C) {
+            serverData = new HashMap<>(); // mapping of server and their user list
+            server = new TreeMap<>(); // mapping of hashval and server name
+            mapServer = new HashMap<>(); //mapping of server name and hashVal
+            ArrayList<Integer> ans = new ArrayList<>();
+            for (int i = 0; i < A.size(); i++) {
+                String type = A.get(i);
+                int hashValue = userHash(B.get(i), C.get(i));
+                switch (type) {
+                    case "ADD":
+                        ans.add(addNewServer(B.get(i), hashValue));
                         break;
-                    }
-                    mp.put(B.get(i),hashValue);
-                    ans.add(addNewServer(machines,hashValue));
-                    break;
-                case "REMOVE":
-                    ans.add(removeServer(machines,mp.get(B.get(i))));
-                    mp.remove(B.get(i));
-                    break;
-                case "ASSIGN":
-                    ans.add(assignKey(machines,hashValue));
-                    break;
-                default:
-                    System.out.println("not a valid input");
-                    break;
+                    case "REMOVE":
+                        ans.add(removeServer(B.get(i)));
+                        break;
+                    case "ASSIGN":
+                        ans.add(assignServer(hashValue));
+                        break;
+                    default:
+                        System.out.println("Invalid input type");
+                        break;
+                }
             }
 
+            return ans;
         }
-        return ans;
 
-    }
-    public int userHash(String username, int hashKey){
-        final long p = hashKey;
-        final long n = 360;
-        long hashCode = 0;
-        long p_pow = 1;
-        for (int i = 0; i < username.length(); i++) {
-            char character = username.charAt(i);
-            hashCode = (hashCode + (character - 'A' + 1) * p_pow) % n;
-            p_pow = (p_pow * p) % n;
-        }
-        return (int)hashCode;
-    }
-
-    public  int addNewServer(List<Set<Integer>> machines, int hashValue){
-        int nextAvailableMachine = getNextAvailableMachine(machines,hashValue);
-        if(nextAvailableMachine == -1){
-            machines.set(hashValue,new HashSet<>());
-            return 0;
-        }
-        Set<Integer> keys = machines.get(nextAvailableMachine);
-//        int left = getPrevAvailableMachine(machines,hashValue);
-//        int right = nextAvailableMachine;
-
-        machines.set(hashValue, new HashSet<>());
-
-        //update the new machine added key from nextAvailableMachine
-        boolean flag = nextAvailableMachine > hashValue;
-        int left = Math.min(nextAvailableMachine,hashValue);
-        int right = Math.max(nextAvailableMachine,hashValue);
-        for(int key:keys){
-            boolean flag2 = key > left && key< right;
-            if(flag && !flag2){
-                machines.get(hashValue).add(key);
+        private Integer addNewServer(String serverName, int hashValue) {
+            //update in map
+            server.put(hashValue,serverName);
+            mapServer.put(serverName,hashValue);
+            // update in serveData map
+            serverData.put(serverName,new ArrayList<>());
+            if(serverData.size() > 0){
+                int nextNumber;
+                if(server.ceilingKey(hashValue+1) == null){
+                    nextNumber = server.firstKey();
+                }else{
+                    nextNumber = server.ceilingKey(hashValue+1);
+                }
+                String nextServer = server.get(nextNumber);
+                List<Integer> list = serverData.get(nextServer);
+                serverData.put(nextServer,new ArrayList<>());
+                for(int i:list){
+                    assignServer(i);
+                }
             }
-            if (!flag && flag2) {
-                machines.get(hashValue).add(key);
+            return serverData.get(serverName).size();
+        }
+        private Integer removeServer(String serverName) {
+            int hashValue = mapServer.get(serverName);
+            if(server.get(hashValue).equals(serverName)){
+                server.remove(hashValue);
             }
-        }
-        for(int key:machines.get(hashValue)){
-            machines.get(nextAvailableMachine).remove(key);
-        }
-//        machines.set(hashValue, keys);
-        //remove the key from the machine
-//        machines.set(nextAvailableMachine, null);
-        return machines.get(hashValue).size();
-
-    }
-
-    public int removeServer(List<Set<Integer>>  machines, int hashValue){
-        // return 0;
-        int nextAvailableMachine = getNextAvailableMachine(machines,hashValue);
-        if(nextAvailableMachine == -1){
-            machines.set(hashValue, null);
-            return 0;
-        }
-        Set<Integer> keys = machines.get(hashValue);
-        //update the keys to nextAvailableMachine from curr machine
-        if(keys==null){
-            return 0;
-        }
-        int cnt = 0;
-        for(int key:keys){
-            cnt++;
-            machines.get(nextAvailableMachine).add(key);
-        }
-//        machines.set(nextAvailableMachine, keys);
-
-        //remove the key from the curr machine
-        machines.set(hashValue,null);
-
-        return cnt;
-    }
-    public int assignKey(List<Set<Integer>>  machines, int hashValue){
-        int nextAvailableMachine = getNextAvailableMachine(machines,hashValue);
-        if(nextAvailableMachine == -1){
-            return -1;
-        }
-        machines.get(nextAvailableMachine).add(hashValue);
-        return hashValue;
-    }
-    public synchronized int getNextAvailableMachine(List<Set<Integer>>  machines, int hashValue){
-        for(int i=hashValue+1;i<360;i++){
-            if(machines.get(i) != null){
-                return i;
+            List<Integer> list = serverData.get(serverName);
+            serverData.remove(serverName);
+            for(int i:list){
+                assignServer(i);
             }
+            return list.size();
         }
-        for(int i=0;i<hashValue;i++){
-            if(machines.get(i) != null){
-                return i;
+        private Integer assignServer(int hashValue) {
+            int nextNumber;
+            if(server.ceilingKey(hashValue) == null){
+                nextNumber = server.firstKey();
+            }else{
+                nextNumber = server.ceilingKey(hashValue);
             }
+            serverData.get(server.get(nextNumber)).add(hashValue);
+            return hashValue;
         }
-        return -1;
 
-    }
 
-    public int getPrevAvailableMachine(List<Set<Integer>> machines, int hashValue) {
-        for(int i=hashValue-1;i>=0;i--){
-            if(machines.get(i) != null){
-                return i;
+        public int userHash(String username, int hashKey) {
+            long p = hashKey;
+            long n = 360;
+            long hashCode = 0;
+            long p_pow = 1;
+            for (int i = 0; i < username.length(); i++) {
+                char character = username.charAt(i);
+                hashCode = (hashCode + (character - 'A' + 1) * p_pow) % n;
+                p_pow = (p_pow * p) % n;
             }
+            return (int)hashCode;
         }
-        for(int i=359;i>hashValue;i--){
-            if(machines.get(i) != null){
-                return i;
-            }
-        }
-        return -1;
-    }
+
 
     public static void main(String[] args) {
         ConsistentHashing consistentHashing = new ConsistentHashing();
